24a8464971d7c2c4493a56c6e17d13bb92a40b4e
Oluwatoyin Atoloye <133176436+Teemzel@users.noreply.github.com>
2026-03-10 11:25:02 +0100

    Add files via upload

---
+++ # 30DaysOfPython Challenge.py
@@ -0,0 +1,27 @@
+#  30DaysOfPython Challenge
+# Day 1
+# Introduction to Python
+
+# Print Hello World!
+print("Hello World!")
+
+print(2 + 4)            # Addition
+print(8 - 5)            # Subtraction
+print(10 / 2)           # Division
+print(8 * 2)            # Multiplication
+print(4 ** 2)           # Exponential
+print(12 // 6)          # Floor division operator
+print(5 % 2)            # Modulus
+
+# Checking Data Type
+
+print(type(5))                  # Int
+print(type(4.5))                # Float
+print(type(True))               #Bool
+print(type("Annaconda"))        # String
+print(type([1,2,3,8]))          # List 
+print(type({3,5.7}))            # Set
+print(type((5,8,9,0)))          # Tuple
+print(type(6 > 7))              # Bool
+print(type({"Language : python"})) # Set
+print(type({"Language" : "Java"})) # Dictionary
