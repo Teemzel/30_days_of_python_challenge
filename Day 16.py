209efeb3697a0d3bfedfb92b4678c6db00ca3238
Oluwatoyin Atoloye <133176436+Teemzel@users.noreply.github.com>
2026-03-25 22:37:27 +0100

    Add files via upload

---
+++ Day 16.py
@@ -0,0 +1,4 @@
+# Exception Handling/ Packing Unpacking Exercises:
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia'] # Unpack the first five countries and store them in a variable nordic_countries, store Estonia and Russia in es, and ru respectively.
+*nordic_countries, es,ru = names
+print(nordic_countries, es,ru)
