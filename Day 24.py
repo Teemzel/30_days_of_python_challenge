a0fb9fe8f28646daa4dc907f5b6481659e2569f6
Oluwatoyin Atoloye <133176436+Teemzel@users.noreply.github.com>
2026-04-02 23:09:40 +0100

    Add files via upload

---
+++ Day 24.py
@@ -0,0 +1,12 @@
+# PIP INSTALLATION
+# pip install pip
+# pip --version
+# pip install --upgrade pip
+# pip install numpy
+# pip install pandas
+# import webbrowser
+# pip list
+# pip show --verbose pandas
+# pip freeze
+# pip install request
+
