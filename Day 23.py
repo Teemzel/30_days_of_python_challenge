6864c0ad52d7a57c2ab7342a6a12f9c58a0d5ade
Oluwatoyin Atoloye <133176436+Teemzel@users.noreply.github.com>
2026-04-02 15:03:54 +0100

    Update Day 23.py

--- Day 23.py
+++ Day 23.py
@@ -7,15 +7,47 @@ def rgb_color_gen(n):
         g = random.randint(0, 255) 
         b = random.randint(0, 255)
         color_gen.append((r,g,b))
-        return color_gen
+    return color_gen
 print(rgb_color_gen(5))
 
 
 import random
 def rgb_color_gen(n):
-        return [( random.randint(0, 255),
+    return [( random.randint(0, 255),
                  random.randint(0, 255), 
-                 random.randint(0, 255) )]
-color = rgb_color_gen(3)
-print(color)
+                 random.randint(0, 255) )
+                 for _ in range(n)]
+print(rgb_color_gen(3))
 # Write a function generate_colors which can generate any number of hexa or rgb colors.
+import random
+import string
+def generate_colors(n, mode = 'hex'):
+    hexa_color = []
+    for _ in range(n):
+        r,g,b = [random.randint(0, 255) for _ in range(3)]
+        if mode == 'rgb':
+            hexa_color.append((r,g,b))
+        else:
+            hexa_color.append(f'#{r:02x}{g:02x}{b:02x}')
+    return hexa_color
+print(generate_colors(3, 'rgb'))
+print(generate_colors(5, 'hex'))
+
+# Call your function shuffle_list, it takes a list as a parameter and it returns a shuffled list
+import random
+def shuffle_list(n):
+    number = n[:]
+    random.shuffle(number)
+    return number
+item = [3, 6, 1, 2]
+list_item = ['book', 'bags', 'shoes', 'clothes', 'jeans']
+shuffle_item = shuffle_list(item)
+shuffle_item2 = shuffle_list(list_item)
+print(shuffle_item)
+print(shuffle_item2)
+# Write a function which returns an array of seven random numbers in a range of 0-9. All the numbers must be unique.
+import random
+def array_numbers():
+    return random.sample(range(10),7)
+print(array_numbers())
+        
